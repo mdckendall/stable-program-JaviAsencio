a9db74fd551e8d7900e046dc2a7eebc6dc06df52
Javier Asencio <108373555+JaviAsencio@users.noreply.github.com>
2022-10-07 02:45:07 +0000

    I edited the stable file to have the string of the address and the new array list for the horse. Additionally, I added information about the horse within the horse file. I also extended the public class so the different sub-classes can get the same properties. For the main file, I tweaked the name s.arrayList.add to s.Horse.add. Ultimately, I also added to the code to allow the input that someone makes to reflect while it starts counting at number one instead of zero.

--- Horse.java
+++ Horse.java
@@ -1,1 +1,36 @@
-//Create the Horse class here.
+// Use "Extend" for the public class so the sub classes have the same properties as the public class.
+public class Horse extends Stable {
+
+  // Information about the horse it is abbreviated to make it easier to read.
+  String name;
+  Double weight;
+  Boolean tame;
+
+  public Horse(String name, Double weight, Boolean tame) {
+
+// Prevent any confusion with the code by using the "this" keyword.
+this.name = name;
+this.weight = weight;
+this.tame = tame;
+
+  // I used get to return the name, weight and whether or not the horse is tamed.
+ 
+  } 
+  // Get and return the name of the horse.
+public String getName() 
+  {
+  return name;
+}
+
+  // Get and return the weight of the horse.
+  public Double getDouble() 
+  {
+    return weight;
+  }
+
+  // Get and return if wheter or not the horse is tamed.
+  public Boolean getTame() 
+  {
+    return tame;
+  }
+}
